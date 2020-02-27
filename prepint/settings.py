c25eeb666862ef825371a34d24a96a65f77c03e2
Alok Nand <aloknand@Aloks-MacBook-Pro.local>
2020-02-27 22:41:37 +0530

    connection to the mysql database

--- prepint/settings.py
+++ prepint/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'prepint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prepintdb',
+        'USER': 'prepint',
+        'PASSWORD': 'Alok!1987',
+        'HOST': '134.209.147.185',
+        'PORT': '3306'
     }
 }
 
